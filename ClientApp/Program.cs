37aff5009add541b811f3943f550a5dce03fe522
Kazuki Ohta <k_ota28@hotmail.com>
2016-08-12 22:28:43 +0900

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using PCLLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new NameProviderProxy().Name);
+        }
+    }
+}
---
+++ PCLLib/NameProviderProxy.cs
@@ -0,0 +1,15 @@
+﻿using NetStandardLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCLLib
+{
+    public class NameProviderProxy
+    {
+        private NameProvider NameProvider { get; } = new NameProvider();
+
+        public string Name => this.NameProvider.Name;
+    }
+}
